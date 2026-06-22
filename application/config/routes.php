44270aa02937476d2e430175673a8bbf7ba28947
gmailtesid-stack <gmailtes.id@gmail.com>
2026-06-22 22:09:16 +0700

    fix: add explicit routes for migrate and berkas controllers

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,6 +52,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 $route['default_controller'] = 'web';
 $route['404_override'] = 'web/error_not_found';
-$route['pendaftaran']  = 'web/pendaftaran';
-$route['logcs']        = 'web/logcs';
+$route['pendaftaran'] = 'web/pendaftaran';
+$route['logcs'] = 'web/logcs';
+$route['migrate/(:any)'] = 'migrate/$1';
+$route['berkas/serve/(:any)/(:any)'] = 'berkas/serve/$1/$2';
 $route['translate_uri_dashes'] = FALSE;
