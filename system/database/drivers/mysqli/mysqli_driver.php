f5a0cb421f858f42cb1d1efd312b62a9ff4d85e5
gmailtesid-stack <gmailtes.id@gmail.com>
2026-06-25 21:58:29 +0700

    Disable global mysqli exception reporting in CI MySQLi driver

--- system/database/drivers/mysqli/mysqli_driver.php
+++ system/database/drivers/mysqli/mysqli_driver.php
@@ -131,6 +131,9 @@ class CI_DB_mysqli_driver extends CI_DB
 
 		$client_flags = ($this->compress === TRUE) ? MYSQLI_CLIENT_COMPRESS : 0;
 		$this->_mysqli = mysqli_init();
+		if (function_exists('mysqli_report')) {
+			mysqli_report(MYSQLI_REPORT_OFF);
+		}
 
 		$this->_mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 10);
 
